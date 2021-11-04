d9e9970297fcbef3eec6452f6df19bd8379d8d5d
Dinopony <dinopony.gg@gmail.com>
2021-11-04 23:44:28 +0100

    Found new properties on entities

--- src/model/entity_on_map.cpp
+++ src/model/entity_on_map.cpp
@@ -7,23 +7,32 @@ Json EntityOnMap::to_json() const
 {
     Json json;
 
+    json["entityType"] = _entity_type->name();
+
     json["posX"] = _pos_x;
     json["posY"] = _pos_y;
     json["posZ"] = _pos_z;
 
-    json["entityType"] = _entity_type->name();
+    json["halfTileX"] = _half_tile_x;
+    json["halfTileY"] = _half_tile_y;
+    json["halfTileZ"] = _half_tile_z;
+
     json["palette"] = _palette;
 
-    json["horizontal_mirror"] = _horizontal_mirror;
-    json["flagUnknown_0_7"] = _flag_unknown_0_7;
     json["fightable"] = _fightable;
     json["liftable"] = _liftable;
+    json["talkable"] = _talkable;
+    json["horizontalMirror"] = _horizontal_mirror;
+    json["appearAfterPlayerMovedAway"] = _appear_after_player_moved_away;
+    json["canPassThrough"] = _can_pass_through;
+
+    json["flagUnknown_0_7"] = _flag_unknown_0_7;
+    json["flagUnknown_6_7"] = _flag_unknown_6_7;
 
     json["byte2Remainder"] = _byte2_remainder;
-    json["byte3"] = _byte3;
+    json["byte3Remainder"] = _byte3_remainder;
     json["byte4"] = _byte4;
-    json["byte6MSH"] = _byte6_msh;
-    json["byte7"] = _byte7;
+    json["byte7"] = _byte7; 
 
     return json;
 }
@@ -36,18 +45,27 @@ EntityOnMap* EntityOnMap::from_json(const Json& json, const World& world)
     entity->_pos_y = json.at("posY");
     entity->_pos_z = json.at("posZ");
 
+    entity->_half_tile_x = json.at("halfTileX");
+    entity->_half_tile_y = json.at("halfTileY");
+    entity->_half_tile_z = json.at("halfTileZ");
+
     entity->_entity_type = world.entity((std::string)json.at("entityType"));
     entity->_palette = json.at("palette");
 
-    entity->_horizontal_mirror = json.at("horizontal_mirror");
+    entity->_horizontal_mirror = json.at("horizontalMirror");
     entity->_flag_unknown_0_7 = json.at("flagUnknown_0_7");
+
     entity->_fightable = json.at("fightable");
     entity->_liftable = json.at("liftable");
-
+    entity->_talkable = json.at("talkable");
     entity->_byte2_remainder = json.at("byte2Remainder");
-    entity->_byte3 = json.at("byte3");
+
+    entity->_can_pass_through = json.at("canPassThrough");
+    entity->_appear_after_player_moved_away = json.at("appearAfterPlayerMovedAway");
+    entity->_flag_unknown_6_7 = json.at("flagUnknown_6_7");
+
+    entity->_byte3_remainder = json.at("byte3Remainder");
     entity->_byte4 = json.at("byte4");
-    entity->_byte6_msh = json.at("byte6MSH");
     entity->_byte7 = json.at("byte7");
 
     return entity;
@@ -72,11 +90,14 @@ EntityOnMap* EntityOnMap::from_rom(const md::ROM& rom, uint32_t addr, const Worl
     uint8_t byte2 = rom.get_byte(addr + 0x2);
     entity->_fightable = byte2 & 0x80;
     entity->_liftable = byte2 & 0x40;
-    entity->_byte2_remainder = byte2 & 0x3F;
+    entity->_talkable = byte2 & 0x20;
+    entity->_byte2_remainder = byte2 & 0x1F;
 
     // Byte 3
     uint8_t byte3 = rom.get_byte(addr + 0x3);
-    entity->_byte3 = byte3;
+    entity->_half_tile_x = byte3 & 0x80;
+    entity->_half_tile_y = byte3 & 0x40;
+    entity->_byte3_remainder = byte3 & 0x3F;
 
     // Byte 4
     entity->_byte4 = rom.get_byte(addr + 0x4);
@@ -87,7 +108,10 @@ EntityOnMap* EntityOnMap::from_rom(const md::ROM& rom, uint32_t addr, const Worl
     // Byte 6
     uint8_t byte6 = rom.get_byte(addr + 0x6);
     entity->_pos_z = byte6 & 0x0F;
-    entity->_byte6_msh = (byte6 & 0xF0) >> 4;
+    entity->_can_pass_through = byte6 & 0x10;
+    entity->_appear_after_player_moved_away = byte6 & 0x20;
+    entity->_half_tile_z = byte6 & 0x40;
+    entity->_flag_unknown_6_7 = byte6 & 0x80;
 
     // Byte 7
     uint8_t byte7 = rom.get_byte(addr + 0x7);
@@ -99,25 +123,26 @@ EntityOnMap* EntityOnMap::from_rom(const md::ROM& rom, uint32_t addr, const Worl
 std::vector<uint8_t> EntityOnMap::to_bytes() const
 {
     // Byte 0
-    uint8_t byte0 = _pos_x;
+    uint8_t byte0 = _pos_x & 0x3F;
     if(_flag_unknown_0_7)
         byte0 |= 0x80;
     if(_horizontal_mirror)
         byte0 |= 0x40;
 
     // Byte 1
-    uint8_t byte1 = _pos_y;
+    uint8_t byte1 = _pos_y & 0x3F;
     byte1 |= ((_palette & 0x3) << 6);
 
     // Byte 2
-    uint8_t byte2 = _byte2_remainder;
-    if(_fightable)
-        byte2 |= 0x80;
-    if(_liftable)
-        byte2 |= 0x40;
+    uint8_t byte2 = _byte2_remainder & 0x1F;
+    if(_fightable)  byte2 |= 0x80;
+    if(_liftable)   byte2 |= 0x40;
+    if(_talkable)   byte2 |= 0x20;
 
     // Byte 3
-    uint8_t byte3 = _byte3;
+    uint8_t byte3 = _byte3_remainder & 0x3F;
+    if(_half_tile_x)   byte3 |= 0x80;
+    if(_half_tile_y)   byte3 |= 0x40;
 
     // Byte 4
     uint8_t byte4 = _byte4;
@@ -126,8 +151,11 @@ std::vector<uint8_t> EntityOnMap::to_bytes() const
     uint8_t byte5 = _entity_type->id();
 
     // Byte 6
-    uint8_t byte6 = _pos_z;
-    byte6 |= (_byte6_msh << 4);
+    uint8_t byte6 = _pos_z & 0x0F;
+    if(_can_pass_through)   byte6 |= 0x10;
+    if(_appear_after_player_moved_away)   byte6 |= 0x20;
+    if(_half_tile_z)        byte6 |= 0x40;
+    if(_flag_unknown_6_7)   byte6 |= 0x80;
 
     // Byte 7
     uint8_t byte7 = _byte7;
--- src/model/entity_on_map.hpp
+++ src/model/entity_on_map.hpp
@@ -10,21 +10,42 @@ class Entity;
 class EntityOnMap
 {
 private:
+    Entity* _entity_type;
+
     uint8_t _pos_x;
     uint8_t _pos_y;
     uint8_t _pos_z;
-    Entity* _entity_type;
+    bool _half_tile_x;
+    bool _half_tile_y;
+    bool _half_tile_z;
+
     uint8_t _palette;
+    
     bool _horizontal_mirror;
-    bool _flag_unknown_0_7;
     bool _fightable;
     bool _liftable;
+    bool _talkable;
+    bool _can_pass_through;
+    bool _appear_after_player_moved_away;
 
+    bool _flag_unknown_0_7;
+    bool _flag_unknown_6_7;
+
+    /// ? bits 0x3F
+    uint8_t _byte3_remainder;
+
+    /// ?
+    uint8_t _byte4;
+
+    /// Has to do with static AI. For orcs, a value of 0 makes them unable to move as long as they have not been hit,
+    /// and a value of 16 makes them perfectly frozen, except for some occasionnal random jumps (swamp shrine boss door guards behavior)
     uint8_t _byte2_remainder;
-    uint8_t _byte3;
-    uint8_t _byte4;    
-    uint8_t _byte6_msh;
-    uint8_t _byte7;
+
+    /// Changing it for enemies change their default AI (0 makes an orc frozen at first, then moves normally once spotted)
+    /// 106 => disappears when all enemies are killed in the room
+    /// 113 => Mir wall barrier 1
+    /// 114 => Mir wall barrier 2
+    uint8_t _byte7; 
 
     // AI behavior?
     // NPC textline?
@@ -35,6 +56,63 @@ public:
 
     Json to_json() const;
     static EntityOnMap* from_json(const Json& json, const World& world);
+
+    uint8_t pos_x() const { return _pos_x; }
+    void pos_x(uint8_t pos_x) { _pos_x = pos_x; }
+
+    uint8_t pos_y() const { return _pos_y; }
+    void pos_y(uint8_t pos_y) { _pos_y = pos_y; }
+
+    uint8_t pos_z() const { return _pos_z; }
+    void pos_z(uint8_t pos_z) { _pos_z = pos_z; }
+
+    bool half_tile_x() const { return _half_tile_x; }
+    void half_tile_x(bool half_tile_x) { _half_tile_x = half_tile_x; }
+
+    bool half_tile_y() const { return _half_tile_y; }
+    void half_tile_y(bool half_tile_y) { _half_tile_y = half_tile_y; }
+
+    bool half_tile_z() const { return _half_tile_z; }
+    void half_tile_z(bool half_tile_z) { _half_tile_z = half_tile_z; }
+
+    Entity* entity_type() const { return _entity_type; }
+    void entity_type(Entity* entity_type) { _entity_type = entity_type; }
+
+    uint8_t palette() const { return _palette; }
+    void palette(uint8_t palette) { _palette = palette; }
+
+    bool horizontal_mirror() const { return _horizontal_mirror; }
+    void horizontal_mirror(bool horizontal_mirror) { _horizontal_mirror = horizontal_mirror; }
+
+    bool flag_unknown_0_7() const { return _flag_unknown_0_7; }
+    void flag_unknown_0_7(bool flag_unknown_0_7) { _flag_unknown_0_7 = flag_unknown_0_7; }
+
+    bool fightable() const { return _fightable; }
+    void fightable(bool fightable) { _fightable = fightable; }
+
+    bool liftable() const { return _liftable; }
+    void liftable(bool liftable) { _liftable = liftable; }
+
+    uint8_t byte2_remainder() const { return _byte2_remainder; }
+    void byte2_remainder(uint8_t byte2_remainder) { _byte2_remainder = byte2_remainder; }
+    
+    uint8_t byte3_remainder() const { return _byte3_remainder; }
+    void byte3_remainder(uint8_t byte3_remainder) { _byte3_remainder = byte3_remainder; }
+    
+    uint8_t byte4() const { return _byte4; }
+    void byte4(uint8_t byte4) { _byte4 = byte4; }
+    
+    bool can_pass_through() const { return _can_pass_through; }
+    void can_pass_through(bool can_pass_through) { _can_pass_through = can_pass_through; }
+
+    bool appear_after_player_moved_away() const { return _appear_after_player_moved_away; }
+    void appear_after_player_moved_away(bool appear_after_player_moved_away) { _appear_after_player_moved_away = appear_after_player_moved_away; }
+
+    bool flag_unknown_6_7() const { return _flag_unknown_6_7; }
+    void flag_unknown_6_7(bool flag_unknown_6_7) { _flag_unknown_6_7 = flag_unknown_6_7; }
+    
+    uint8_t byte7() const { return _byte7; }
+    void byte7(uint8_t byte7) { _byte7 = byte7; }
 
     static EntityOnMap* from_rom(const md::ROM& rom, uint32_t addr, const World& world);
     std::vector<uint8_t> to_bytes() const;
