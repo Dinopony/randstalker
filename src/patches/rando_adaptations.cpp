187dc44f89d50934ed66480b0519aa7d273368aa
Dinopony <dinopony.gg@gmail.com>
2021-11-09 00:30:30 +0100

    Used unkillable flag to normalize bosses HP

--- src/patches/rando_adaptations.cpp
+++ src/patches/rando_adaptations.cpp
@@ -396,25 +396,49 @@ void prevent_hint_item_save_scumming(md::ROM& rom)
 //      ORIGINAL GAME BUGS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void fix_tree_cutting_glitch(md::ROM& rom)
+/**
+ * The original game has no way to make enemies unkillable, so it uses weird ultra high HP bosses
+ * which don't represent their real life pool. This injection modifies the way this works to improve
+ * this part of the engine by implementing a way to make them unkillable.
+ * If specified in the options, this function also adds the check to prevent sacred trees from being
+ * killed by feeding them money.
+ */
+void improve_checks_before_kill(md::ROM& rom, const RandomizerOptions& options)
 {
+    constexpr uint32_t ADDR_JMP_KILL = 0x16284;
+    constexpr uint32_t ADDR_JMP_NO_KILL_YET = 0x16262;
+    constexpr uint32_t ADDR_JMP_NO_KILL = 0x1627C;
+    
     // Inject a new function which fixes the money value check on an enemy when it is killed, causing the tree glitch to be possible
-    md::Code func_fix_tree_cutting_glitch;
+    md::Code func_check_before_kill;
  
-    func_fix_tree_cutting_glitch.tstb(addr_(reg_A5, 0x36));  // tst.b ($36,A5) [4A2D 0036]
-    func_fix_tree_cutting_glitch.beq(4);
-        // Only allow the "killable because holding money" check if the enemy is not a tree
-        func_fix_tree_cutting_glitch.cmpiw(0x126, addr_(reg_A5, 0xA));
-        func_fix_tree_cutting_glitch.beq(2);
-            func_fix_tree_cutting_glitch.jmp(0x16284);
-    func_fix_tree_cutting_glitch.rts();
+    // If enemy doesn't hold money, no kill yet
+    func_check_before_kill.tstb(addr_(reg_A5, 0x36));
+    if(options.fix_tree_cutting_glitch())
+    {
+        func_check_before_kill.beq(4);
+            // Only allow the "killable because holding money" check if the enemy is not a tree
+            func_check_before_kill.cmpiw(0x126, addr_(reg_A5, 0xA));
+            func_check_before_kill.beq(2);
+                func_check_before_kill.jmp(ADDR_JMP_KILL);
+    }
+    else
+    {
+        func_check_before_kill.beq(2);
+            func_check_before_kill.jmp(ADDR_JMP_KILL);
+    }
+    // If enemy has "empty item" as a drop, make it unkillable
+    func_check_before_kill.cmpib(0x3F, addr_(reg_A5, 0x77));
+    func_check_before_kill.bne(3);
+        func_check_before_kill.movew(0x0001, addr_(reg_A5, 0x3E));
+        func_check_before_kill.rts();
+    func_check_before_kill.jmp(ADDR_JMP_NO_KILL_YET);
 
-    uint32_t func_addr = rom.inject_code(func_fix_tree_cutting_glitch);
+    uint32_t func_addr = rom.inject_code(func_check_before_kill);
 
     // Call the injected function when killing an enemy
-    rom.set_code(0x01625C, md::Code().jsr(func_addr));
+    rom.set_code(0x01625C, md::Code().jmp(func_addr));
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -440,8 +464,6 @@ void patch_rando_adaptations(md::ROM& rom, const RandomizerOptions& options, con
     fix_mir_tower_priest_room_items(rom);
     prevent_hint_item_save_scumming(rom);
 
-    // Fix original game bugs
-    if(options.fix_tree_cutting_glitch())
-        fix_tree_cutting_glitch(rom);
+    improve_checks_before_kill(rom, options);
 }
 
