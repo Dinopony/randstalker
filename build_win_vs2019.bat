8bd21cf46a77138201567ae7623c0d30f1b3fcbf
Dinopony <dinopony.gg@gmail.com>
2021-12-08 17:57:46 +0100

    Improved build scripts to work in more cases

--- build_win_vs2019.bat
+++ build_win_vs2019.bat
@@ -1,4 +1,4 @@
-git submodule update --recursive
+git submodule update --init --recursive
 
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -B "./build_debug" -G "Visual Studio 16 2019" -A x64
 cmake --build ./build_debug --config Debug
