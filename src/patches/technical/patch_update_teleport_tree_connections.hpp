21e03ccfada2e943c7bf22fa3d5a5f15e7893b45
Dinopony <dinopony.gg@gmail.com>
2023-04-26 09:28:52 +0200

    Fixed bugged teleport tree connections

--- src/patches/technical/patch_update_teleport_tree_connections.hpp
+++ src/patches/technical/patch_update_teleport_tree_connections.hpp
@@ -17,25 +17,39 @@ public:
     {
         const RandomizerWorld& randomizer_world = reinterpret_cast<RandomizerWorld&>(world);
 
+        std::vector<MapConnection*> map_connection_pool;
         for(const auto& pair : randomizer_world.teleport_tree_pairs())
         {
             WorldTeleportTree* tree_1 = pair.first;
             WorldTeleportTree* tree_2 = pair.second;
 
-            // If the pair is unchanged compared to vanilla, no need to do anything
-            if(tree_1->paired_map_id() == tree_2->map_id())
-                continue;
-
-            // Update connections between tree maps
             for(MapConnection* conn : world.map_connections(tree_1->map_id(), tree_1->paired_map_id()))
-                conn->replace_map(tree_1->paired_map_id(), tree_2->map_id());
+                if(!vectools::contains(map_connection_pool, conn))
+                    map_connection_pool.emplace_back(conn);
+
             for(MapConnection* conn : world.map_connections(tree_2->map_id(), tree_2->paired_map_id()))
-                conn->replace_map(tree_2->paired_map_id(), tree_1->map_id());
+                if(!vectools::contains(map_connection_pool, conn))
+                    map_connection_pool.emplace_back(conn);
+        }
+
+        uint8_t i=0;
+        for(const auto& pair : randomizer_world.teleport_tree_pairs())
+        {
+            WorldTeleportTree* tree_1 = pair.first;
+            WorldTeleportTree* tree_2 = pair.second;
 
             tree_1->paired_map_id(tree_2->map_id());
             tree_2->paired_map_id(tree_1->map_id());
-        }
 
+            // Pick map connections from the connection pool and set both map_ids to both ends of the pair
+            MapConnection* conn_1 = map_connection_pool[i++];
+            conn_1->map_id_1(tree_1->map_id());
+            conn_1->map_id_2(tree_2->map_id());
+
+            MapConnection* conn_2 = map_connection_pool[i++];
+            conn_2->map_id_1(tree_2->map_id());
+            conn_2->map_id_2(tree_1->map_id());
+        }
     }
 };
 
