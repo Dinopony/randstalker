f982dabb4eb019bc992ebea58d7371d7442ce00d
Dinopony <dinopony.gg@gmail.com>
2023-10-05 09:36:27 +0200

    Fixed teleport tree flags being broken in shuffle trees + visit trees

--- src/logic_model/world_teleport_tree.hpp
+++ src/logic_model/world_teleport_tree.hpp
@@ -3,8 +3,10 @@
 #include <cstdint>
 #include <string>
 #include <utility>
+#include <sstream>
 #include <landstalker-lib/md_tools.hpp>
 #include <landstalker-lib/tools/json.hpp>
+#include <landstalker-lib/tools/flag.hpp>
 
 class WorldTeleportTree {
 private:
@@ -12,12 +14,14 @@ private:
     uint16_t _map_id;
     uint16_t _paired_map_id = 0xFFFF;
     std::string _node_id;
+    Flag _flag;
 
 public:
-    WorldTeleportTree(std::string name, uint16_t tree_map_id, std::string node_id) :
+    WorldTeleportTree(std::string name, uint16_t tree_map_id, std::string node_id, Flag flag) :
         _name       (std::move(name)),
         _map_id     (tree_map_id),
-        _node_id    (std::move(node_id))
+        _node_id    (std::move(node_id)),
+        _flag       (std::move(flag))
     {}
 
     [[nodiscard]] const std::string& name() const { return _name; }
@@ -28,6 +32,7 @@ public:
     void paired_map_id(uint16_t map_id) { _paired_map_id = map_id; }
 
     [[nodiscard]] const std::string& node_id() const { return _node_id; }
+    [[nodiscard]] const Flag& flag() const { return _flag; }
 
     [[nodiscard]] Json to_json() const
     {
@@ -35,6 +40,14 @@ public:
         json["name"] = _name;
         json["treeMapId"] = _map_id;
         json["nodeId"] = _node_id;
+
+        std::stringstream ss;
+        ss << std::hex << _flag.byte;
+        json["flag"] = {
+            { "byte", "0x" + ss.str() },
+            { "bit", _flag.bit }
+        };
+
         return json;
     }
 
@@ -44,6 +57,13 @@ public:
         uint16_t tree_map_id = json.at("treeMapId");
         std::string node_id = json.at("nodeId");
 
-        return new WorldTeleportTree(name, tree_map_id, node_id);
+        uint16_t byte = 0;
+        std::string byte_string = json.at("flag").at("byte");
+        std::stringstream ss;
+        ss << std::hex << byte_string.substr(2);
+        ss >> byte;
+        uint8_t bit = json.at("flag").at("bit");
+
+        return new WorldTeleportTree(name, tree_map_id, node_id, Flag(byte, bit));
     }
 };
--- src/patches/technical/patch_update_teleport_tree_connections.hpp
+++ src/patches/technical/patch_update_teleport_tree_connections.hpp
@@ -13,9 +13,16 @@
 class PatchUpdateTeleportTreeConnections : public GamePatch
 {
 public:
+    void alter_rom(md::ROM& rom) override
+    {
+        // Instead of making the tree flags work by pair and test both when entering a tree, only check if the
+        // opposite tree's flag is set
+        rom.set_code(0x4E74, md::Code().nop(4));
+    }
+
     void alter_world(World& world) override
     {
-        const RandomizerWorld& randomizer_world = reinterpret_cast<RandomizerWorld&>(world);
+        RandomizerWorld& randomizer_world = reinterpret_cast<RandomizerWorld&>(world);
 
         // 1) Build a catalog of original tree map connections that are usable to represent the new tree connections
         std::vector<MapConnection*> map_connection_pool;
@@ -24,11 +31,11 @@ public:
             WorldTeleportTree* tree_1 = pair.first;
             WorldTeleportTree* tree_2 = pair.second;
 
-            for(MapConnection* conn : world.map_connections(tree_1->map_id(), tree_1->paired_map_id()))
+            for(MapConnection* conn : randomizer_world.map_connections(tree_1->map_id(), tree_1->paired_map_id()))
                 if(!vectools::contains(map_connection_pool, conn))
                     map_connection_pool.emplace_back(conn);
 
-            for(MapConnection* conn : world.map_connections(tree_2->map_id(), tree_2->paired_map_id()))
+            for(MapConnection* conn : randomizer_world.map_connections(tree_2->map_id(), tree_2->paired_map_id()))
                 if(!vectools::contains(map_connection_pool, conn))
                     map_connection_pool.emplace_back(conn);
         }
@@ -61,6 +68,32 @@ public:
             conn_2->map_id_1(tree_2->map_id());
             conn_2->map_id_2(tree_1->map_id());
         }
+    }
+
+    void inject_code(md::ROM& rom, World& world) override
+    {
+        constexpr uint32_t TELEPORT_TREE_FLAGS_TABLE_ADDR = 0x5102;
+        ByteArray bytes;
+
+        RandomizerWorld& randomizer_world = reinterpret_cast<RandomizerWorld&>(world);
+        for(const auto& pair : randomizer_world.teleport_tree_pairs())
+        {
+            WorldTeleportTree* tree_1 = pair.first;
+            WorldTeleportTree* tree_2 = pair.second;
+
+            bytes.add_word(tree_1->map_id());
+            bytes.add_byte((uint8_t)tree_2->flag().byte);
+            bytes.add_byte(tree_2->flag().bit);
+
+            bytes.add_word(tree_2->map_id());
+            bytes.add_byte((uint8_t)tree_1->flag().byte);
+            bytes.add_byte(tree_1->flag().bit);
+        }
+        bytes.add_word(0xFFFF);
+
+        if(bytes.size() != 0x2A)
+            throw LandstalkerException("Teleport tree flags array has a different size from vanilla game.");
+        rom.set_bytes(TELEPORT_TREE_FLAGS_TABLE_ADDR, bytes);
     }
 };
 
